411d4bfcd5cf395d817ecffd01ce9d8e516be550
Genataa <ganchevevgeni@abv.bg>
2026-02-25 13:02:04 +0200

    calendar languafe

---
+++ TODOlistsystem/Controllers/LanguageController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TODOlistsystem.Controllers
+{
+    public class LanguageController : Controller
+    {
+        public IActionResult SetLanguage(string lang, string returnUrl)
+        {
+            if (!string.IsNullOrEmpty(lang))
+            {
+                Response.Cookies.Append(
+                    CookieRequestCultureProvider.DefaultCookieName,
+                    CookieRequestCultureProvider.MakeCookieValue(
+                        new RequestCulture(lang)),
+                    new CookieOptions
+                    {
+                        Expires = DateTimeOffset.UtcNow.AddYears(1)
+                    });
+            }
+
+            return LocalRedirect(returnUrl);
+        }
+    }
+}
