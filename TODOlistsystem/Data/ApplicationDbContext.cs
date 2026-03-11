a4efe7cc718648db4138c908f699cf87bbb96ade
DarinKolev <koravot70@gmail.com>
2026-03-11 12:53:22 +0200

    Updated Notes, made applicationuser class, updated dbcontext

--- TODOlistsystem/Data/ApplicationDbContext.cs
+++ TODOlistsystem/Data/ApplicationDbContext.cs
@@ -1,13 +1,23 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TODOlistsystem.Models;
 
 namespace TODOlistsystem.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
-    {
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser> {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
+            : base(options) {
+        }
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Note>(entity => {
+                entity.HasIndex(n => n.UserId);
+                entity.HasIndex(n => n.IsDeleted);
+                entity.HasIndex(n => n.DueDate);
+            });
         }
     }
 }
---
+++ TODOlistsystem/Models/Note.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TODOlistsystem.Models;
+
+public class Note
+{
+
+    [Key]
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    [Required]
+    [StringLength(5000)]
+    public string Content { get; set; } = string.Empty;
+
+    public bool IsActive { get; set; } = true;
+    public bool IsCompleted { get; set; } = false;
+    public bool IsDeleted { get; set; } = false;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? DueDate { get; set; }
+
+    [Required]
+    public string UserId { get; set; } = null!;
+
+    [ForeignKey("UserId")]
+    public virtual ApplicationUser User { get; set; } = null!;
+
+    public string? SharedWithUserId { get; set; }
+    public string? AssignedToUserId { get; set; }
+}
